af9c832540c4d9d8a6202ce2031e793c2febe7ee
Vitor Wallace <vitor.wallace@hotmail.com>
2022-05-24 14:53:37 -0300

    feat(ZEMA-2254): (new auth and consent endpoints)

---
+++ lib/m2y_tecban/constants/constants.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+module M2yTecban
+  ### Base paths ###
+  BASE_HH = 'https://hh-pub.'
+  BASE_CM = 'https://cm-pub.'
+  BASE_CONSENT = 'https://pub-consent.'
+
+  AUTH_PATH = '/auth/'
+  CONSENT_URL = '/v2.2/consents/'
+
+end
--- lib/m2y_tecban/modules/openbanking.rb
+++ lib/m2y_tecban/modules/openbanking.rb
@@ -1,5 +1,13 @@
 module M2yTecban
   class Openbanking
-    
+    # https://hh-pub.zem.hml.sandbox-tecban.com.br/auth/
+    def self.get_auth(body)
+      get(BASE_HH + base_url + AUTH_PATH, parsed_body(body))
+    end
+
+    # https://cm-pub.zem.hml.sandbox-tecban.com.br/v2.2/consents/
+    def self.get_consent(body)
+      post(BASE_CM + base_url + CONSENT_URL, parsed_body(body))
+    end
   end
 end
